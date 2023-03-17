91d7b7715a3c864bec271410beddb3db057380ac
Andreina De Sola <andreinadesola@gmail.com>
2023-03-17 00:47:41 -0300

    social fix

--- src/components/Navbar/Social/Social.jsx
+++ src/components/Navbar/Social/Social.jsx
@@ -11,22 +11,22 @@ export const Social = () => {
                 <h6>¿qué esperas para hornear tus ideas?</h6>
             </div>
             <div className="social-icons-wrapper">
-                <a href="https://www.instagram.com/" target="_blank">
+                <a href="https://www.instagram.com/" target="_blank" rel="noreferrer">
                     <motion.div whileHover={{ scale: 1.2 }} whileTap={{ scale: 0.9 }} className='social-icon'>
                         <img className='icon-s' src={instagram} alt="instagram" />
                     </motion.div>
                 </a>
-                <a href="https://www.linkedin.com/" target="_blank">
+                <a href="https://www.linkedin.com/" target="_blank" rel="noreferrer">
                     <motion.div whileHover={{ scale: 1.2 }} whileTap={{ scale: 0.9 }} className='social-icon'>
                         <img className='icon-s' src={linkedin} alt="linkedin" />
                     </motion.div>
                 </a>
-                <a href="https://www.tiktok.com/" target="_blank">
+                <a href="https://www.tiktok.com/" target="_blank" rel="noreferrer">
                     <motion.div whileHover={{ scale: 1.2 }} whileTap={{ scale: 0.9 }} className='social-icon'>
                         <img className='icon-s' src={tiktok} alt="tiktok" />
                     </motion.div>
                 </a>
-                <a href="https://wa.me/54911XXXXXXXX" target="_blank">
+                <a href="https://wa.me/54911XXXXXXXX" target="_blank" rel="noreferrer">
                     <motion.div whileHover={{ scale: 1.2 }} whileTap={{ scale: 0.9 }} className='social-icon'>
                         <img className='icon-s' src={chat} alt="chat" />
                     </motion.div>
