d6352ab2accd9ba18cdd4478305527983b52b0a8
Andreina De Sola <andreinadesola@gmail.com>
2023-03-17 23:30:04 -0300

    Fixes idioma

---
+++ src/components/Navbar/LanguageSelect/LanguageSelect.jsx
@@ -0,0 +1,11 @@
+import { useContext } from "react";
+import LanguageContext from '../../../Context/LanguageContext';
+export const LanguageSelect = () => {
+    const { handleLanguage } = useContext(LanguageContext);
+    return (
+        <select className="mobile-hidden" name="language" onChange={handleLanguage}>
+            <option value="es">ES</option>
+            <option value="en">EN</option>
+        </select>
+    );
+}
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -7,22 +7,20 @@ import { Link } from 'react-router-dom';
 import { motion } from "framer-motion";
 import { Social } from './Social/Social';
 import LanguageContext from '../../Context/LanguageContext';
+import { LanguageSelect } from './LanguageSelect/LanguageSelect';
+
 
 export const Navbar = () => {
     const [navOpen, setNavOpen] = useState(false);
-    const { texts, handleLanguage } = useContext(LanguageContext);
+    const { texts} = useContext(LanguageContext);
 
     return (
         <>
             <div className="nav">
                 <div className="navbar">
                     <Link to={"/"} onClick={() => navOpen && setNavOpen(false)}> <img className="margin-navbar logo" src={navOpen ? `${logoNav}` : `${logo}`} alt="logo" /></Link>
-
                     <div className='container-nav-r'>
-                        <select name="language" onChange={handleLanguage}>
-                            <option value="es">ES</option>
-                            <option value="en">EN</option>
-                        </select>
+                        <LanguageSelect/>
                         <div className="menu-toggle" onClick={() => setNavOpen(!navOpen)}>
                             <motion.div whileHover={{ scale: 1.2 }} whileTap={{ scale: 0.9 }} className={`margin-navbar menu-container ${navOpen ? 'm-open' : 'm-close'}`}>
                                 <img className="menu" src={navOpen ? `${close}` : `${menu}`} alt="menu" />
