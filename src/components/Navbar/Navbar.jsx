beccda9e7ff11accc34c85171a6a3decace34c5e
Andreina De Sola <andreinadesola@gmail.com>
2023-03-21 15:23:26 -0300

    anim links

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -12,7 +12,7 @@ import { LanguageSelect } from './LanguageSelect/LanguageSelect';
 
 export const Navbar = () => {
     const [navOpen, setNavOpen] = useState(false);
-    const { texts} = useContext(LanguageContext);
+    const { texts } = useContext(LanguageContext);
 
     return (
         <>
@@ -20,7 +20,7 @@ export const Navbar = () => {
                 <div className="navbar">
                     <Link to={"/"} onClick={() => navOpen && setNavOpen(false)}> <img className="margin-navbar logo" src={navOpen ? `${logoNav}` : `${logo}`} alt="logo" /></Link>
                     <div className='container-nav-r'>
-                        <LanguageSelect/>
+                        <LanguageSelect />
                         <div className="menu-toggle" onClick={() => setNavOpen(!navOpen)}>
                             <motion.div whileHover={{ scale: 1.2 }} whileTap={{ scale: 0.9 }} className={`margin-navbar menu-container ${navOpen ? 'm-open' : 'm-close'}`}>
                                 <img className="menu" src={navOpen ? `${close}` : `${menu}`} alt="menu" />
@@ -36,10 +36,10 @@ export const Navbar = () => {
                 }}>
                     <div className="container-secciones">
                         <ul>
-                            <Link onClick={() => setNavOpen(!navOpen)} to={"/"}> <li>{texts.menuCat1}</li> </Link>
-                            <Link onClick={() => setNavOpen(!navOpen)} to={"/about"}><li>{texts.menuCat2}</li></Link>
-                            <Link onClick={() => setNavOpen(!navOpen)} to={"/services"}><li>{texts.menuCat3}</li></Link>
-                            <Link onClick={() => setNavOpen(!navOpen)} to={"/contact"}><li>{texts.menuCat4}</li></Link>
+                            <Link onClick={() => setNavOpen(!navOpen)} to={"/"}> <motion.li initial={{ opacity: 0 }} whileInView={{ opacity: 1 }} transition={{ ease: "easeOut", duration: .4 }} viewport={{ once: true }}>{texts.menuCat1}</motion.li> </Link>
+                            <Link onClick={() => setNavOpen(!navOpen)} to={"/about"}><motion.li initial={{ opacity: 0 }} whileInView={{ opacity: 1 }} transition={{ ease: "easeOut", duration: .4, delay: .4 }} viewport={{ once: true }}>{texts.menuCat2}</motion.li></Link>
+                            <Link onClick={() => setNavOpen(!navOpen)} to={"/services"}><motion.li initial={{ opacity: 0 }} whileInView={{ opacity: 1 }} transition={{ ease: "easeOut", duration: .4, delay: .8  }} viewport={{ once: true }}>{texts.menuCat3}</motion.li></Link>
+                            <Link onClick={() => setNavOpen(!navOpen)} to={"/contact"}><motion.li initial={{ opacity: 0 }} whileInView={{ opacity: 1 }} transition={{ ease: "easeOut", duration: .4, delay: 1.2  }} viewport={{ once: true }}>{texts.menuCat4}</motion.li></Link>
                         </ul>
                     </div>
                     <Social />
